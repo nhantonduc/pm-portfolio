cd3bb4cb37bb5b76688dce60a2ccc21a1ca09798
Nhan Ton <nhantonduc@gmail.com>
2024-01-13 17:26:04 -0500

    experience page almost finished

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -1,8 +1,49 @@
-import React from 'react'
+import React from 'react';
+import { VerticalTimeline, VerticalTimelineElement} from 'react-vertical-timeline-component';
+import "react-vertical-timeline-component/style.min.css";
+import SchoolIcon from "@mui/icons-material/School";
+import WorkIcon from "@mui/icons-material/Work";
+import "../styles/experience.css";
 
 function Experience() {
   return (
-    <div>Experience</div>
+    <div className="experience">
+        <VerticalTimeline lineColor="#3e497a">
+            <VerticalTimelineElement 
+                className="vertical-timeline-element--education"
+                date="2010 - 2014"
+                iconStyle={{background:"#3e497a", color: "#fff"}}
+                icon={<WorkIcon/>}
+            >
+                <h3 className="vertical-timeline-element-title">
+                    Boston University, MA
+                </h3>
+                <p>...CV Bullet Points...</p>
+            </VerticalTimelineElement>
+            <VerticalTimelineElement 
+                className="vertical-timeline-element--education"
+                date="2010 - 2014"
+                iconStyle={{background:"#3e497a", color: "#fff"}}
+                icon={<WorkIcon/>}
+            >
+                <h3 className="vertical-timeline-element-title">
+                    Boston University, MA
+                </h3>
+                <p>...CV Bullet Points...</p>
+            </VerticalTimelineElement> 
+            <VerticalTimelineElement 
+                className="vertical-timeline-element--education"
+                date="2010 - 2014"
+                iconStyle={{background:"#3e497a", color: "#fff"}}
+                icon={<WorkIcon/>}
+            >
+                <h3 className="vertical-timeline-element-title">
+                    Boston University, MA
+                </h3>
+                <p>...CV Bullet Points...</p>
+            </VerticalTimelineElement>           
+        </VerticalTimeline>        
+    </div>
   )
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import EmailIcon from "@mui/icons-material/Email";
+import "../styles/home.css";
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className="home">
+        <div className="about">
+            <h2>Hi, my name is Nhan!</h2>
+            <div className="prompt">
+                <p>insert description</p>
+                <LinkedInIcon/>
+                <EmailIcon/>
+            </div>
+        </div>
+    </div>
   )
 }
 
